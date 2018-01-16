b26af2d30aa793f9412c7913495335b2c4bdd5ba
NinthG <294198129@qq.com>
2018-01-17 01:15:51 +0800

    :memo: demo提交

---
+++ Mission/Mission-18/main.js
@@ -0,0 +1,14 @@
+// 按下鼠标
+document.onmousedown = function(x){
+    console.log(x)
+}
+
+// 动鼠标
+document.onmousemove = function(y){
+    console.log(y)
+}
+
+// 松开鼠标
+document.onmouseout = function(z){
+    console.log(z)
+}
